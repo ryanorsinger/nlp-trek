bac9dc241ba710c9d0b3316e7a9d64c47691d085
Ryan Orsinger <ryan.orsinger@gmail.com>
2021-04-30 19:59:36 -0500

    add TNG acquisition and csv export

--- wrangle.py
+++ wrangle.py
@@ -22,7 +22,7 @@ def get_dates(soup):
     }
 
 
-# TODO: this is_dialog has a slight issue with the first line after the captain's log intro if we split on " \r\n"
+# TODO: this is_dialog has a slight issue with the first line after the captain's log intro if we split on " \r\n" in the get_episode function
 def is_dialog(string):
     """
     Dialog lines start with one or more strings of all capitals and end with
@@ -61,11 +61,37 @@ def get_episode(url):
     lines = [line for line in raw_lines if is_dialog(line)]
     df["lines"] = lines
     
-    dates = get_dates(soup)
-    df["original_airdate"] = dates["original_airdate"].replace("Original Airdate:", "")
-    df["stardate"] = dates["stardate"]
+    # TODO: Troubleshoot why some episode urls don't conform to this pattern and there's an out of index issue
+    # dates = get_dates(soup)
+    # df["original_airdate"] = dates["original_airdate"].replace("Original Airdate:", "")
+    # df["stardate"] = dates["stardate"]
     
     df["episode_name"] = get_episode_name(soup)
     
     return df
+
+
+
+def get_all_episode_urls(listing_url):
+    response = requests.get(listing_url)
+    soup = BeautifulSoup(response.text)
+    links = soup.select("a")
+    episode_urls = links[:-2]
+    urls = [tag["href"] for tag in links]
+    
+    next_gen = "http://www.chakoteya.net/NextGen/"
+    urls = [next_gen + url for url in urls]
+
+    # Last 2 urls are not episode links
+    urls = urls[:-2]
+    urls
+    episodes = []
+    for url in urls:
+        episode = get_episode(url)
+        print(f"Processing episode from {url}")
+        print("-----")
+        episodes.append(episode)
+
+    all_episodes = pd.concat(episodes)
+    return all_episodes
 
